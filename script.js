84b42a1062d278195c085effe41f37a96099aa24
davideerusso <rd.96.rd@gmail.com>
2025-04-03 17:30:48 +0200

    end

--- script.js
+++ script.js
@@ -19,9 +19,29 @@ for (let i = 0; i < teachers.length; i++) {
 console.log(longNames);
 // 3. Rimuovi 'Ed' dall'array teachers
 
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === "Ed") {
+    teachers.splice(i, 1);
+  }
+}
+console.log(teachers);
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let isFabioPresent = false;
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === "Fabio") {
+    isFabioPresent = true;
+  }
+}
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = "";
+
+for (let i = 0; i < teachers.length; i++) {
+  teachersString += teachers[i] + "," + " ";
+}
+
+console.log(teachersString);
